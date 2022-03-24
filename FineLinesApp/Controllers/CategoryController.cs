bd44872b5b8203ea00212fff8c276983424c0803
Neville <neville.rabakale@gmail.com>
2022-03-24 12:20:42 +0100

    Implemented Edit/update for Category

--- FineLinesApp/Controllers/CategoryController.cs
+++ FineLinesApp/Controllers/CategoryController.cs
@@ -47,8 +47,50 @@ namespace FineLinesApp.Controllers
                 return RedirectToAction("Index");
             }
             return View(obj);
+        }
 
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            //check for invalid id
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
 
+            var categoryFromDb = _db.Categories.Find(id);
+            //check if item from Db has valid Id
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //POST --Addind items to Categories
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+
+            //check if item from Db has valid Id
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "DisplayOrder and Name cannot match ");
+            }
+
+            //Validation
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                //this takes you back to the index.
+                //-- If you want to redirect to action in another controller,
+                //you can just add controller name as second variable
+                return RedirectToAction("Index");
+            }
+            return View(obj);
         }
     }
 }
