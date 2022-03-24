7ca6604c2d5f6846e9a3bb1ea2f46278141ff5f6
Neville <neville.rabakale@gmail.com>
2022-03-24 08:47:49 +0100

    Add project files.

---
+++ FineLinesApp/Controllers/CategoryController.cs
@@ -0,0 +1,42 @@
+﻿using FineLinesApp.Data;
+using FineLinesApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FineLinesApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
+        public IActionResult Index()
+        {
+            //retrieve from db as list and pass to the CategotryList object
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST --Addind items to Categories
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            //this takes you back to the index.
+            //-- If you want to redirect to action in another controller,
+            //you can just add controller name as second variable
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ FineLinesApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using FineLinesApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FineLinesApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        //Create dataset -> DataTable for "Categories" of Category Model
+        public DbSet<Category> Categories { get; set; }
+    }
+}
