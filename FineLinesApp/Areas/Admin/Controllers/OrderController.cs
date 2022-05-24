3f93fbdf03bdcdd42eb3967287bbc754aa0f77a1
Neville <neville.rabakale@gmail.com>
2022-05-24 13:25:04 +0200

    Implemented Display Orders. This all displays orders as a list. We are using dataTables to display them.

---
+++ FineLinesApp/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,36 @@
+﻿using FineLines.DataAccess.Repositories;
+using FineLines.DataAccess.Repositories.IRepositories;
+using FineLines.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+
+namespace FineLinesApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+        [HttpGet]
+        //call to retrieve datatable/all items in database-
+        //ret json because it is for the "datatables.net datatable
+        public IActionResult GetAll()
+        {
+            IEnumerable<OrderHeader> orderHeaders;
+
+            orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+            return Json(new { data = orderHeaders });
+        }
+        #endregion
+    }
+}
