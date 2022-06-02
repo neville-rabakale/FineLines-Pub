dbf5d3123fc2f9f66b8e17999065a385bd659d4e
Neville <neville.rabakale@gmail.com>
2022-06-02 15:20:40 +0200

    Updated Register to only allow Admin to Create Admin.

--- FineLinesApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ FineLinesApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -219,7 +219,15 @@ namespace FineLinesApp.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        if (User.IsInRole(SD.Role_Admin))
+                        {
+                            TempData["success"] = "New Admin created Successfully";
+                        }
+                        else
+                        {
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+
+                        }
                         return LocalRedirect(returnUrl);
                     }
                 }
