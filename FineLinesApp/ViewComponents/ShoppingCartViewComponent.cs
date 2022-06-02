aed25bfa6f09cd8fdd9b4eb69247301b3ae8742b
Neville <neville.rabakale@gmail.com>
2022-06-02 11:16:47 +0200

    Implemneted ShoppingCart Component to be able to solve the Shopping Cart Session update problem. Shopping cart now works as it should.

---
+++ FineLinesApp/ViewComponents/ShoppingCartViewComponent.cs
@@ -0,0 +1,47 @@
+﻿using FineLines.DataAccess.Repositories.IRepositories;
+using FineLines.Utility;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace FineLinesApp.ViewComponents
+{
+    public class ShoppingCartViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ShoppingCartViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            //check for signed in user
+            if (claim != null)
+            {
+                //check if session exists
+                if (HttpContext.Session.GetInt32(SD.SessionCart) != null)
+                {
+                    return View(HttpContext.Session.GetInt32(SD.SessionCart));
+                }
+                //if session doesnt exist check db
+                var count = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value).ToList().Count;
+                HttpContext.Session.SetInt32(SD.SessionCart, count);
+                return View(HttpContext.Session.GetInt32(SD.SessionCart));
+
+            }
+            else
+            {
+                //If we come here, user has either not signed in or if user signs out 
+                HttpContext.Session.Clear();
+                return View(0);
+
+            }
+
+
+        }
+
+    }
+}
